4e4b127770eb04271053d8b0b859a161110c5329
leellun <leellun@sina.cn>
2022-11-27 23:31:04 +0800

    timeout

---
+++ cloud-oauth2/uua/src/main/java/com/newland/uua/UuaApplication.java
@@ -0,0 +1,13 @@
+package com.newland.uua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UuaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UuaApplication.class, args);
+    }
+
+}
